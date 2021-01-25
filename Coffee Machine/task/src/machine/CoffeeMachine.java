8975a41e3f130f6aea9024fb1785e95c72680496
Zaid Neurothrone <zaid_neurothrone@hotmail.com>
2021-01-25 19:57:24 +0100

    Rename variables to conform to standard for constants

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -2,19 +2,26 @@ package machine;
 
 import java.util.Scanner;
 
+/**
+ * The coffee machine class calculates how much of each
+ * ingredient necessary to make a coffee cup is required
+ * for a specified amount of cups
+ * @author Zaid Neurothrone
+ */
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        int waterPerCup = 200;
-        int milkPerCup = 50;
-        int coffeeBeansPerCup = 15;
+        final int WATER_PER_CUP = 200;
+        final int MILK_PER_CUP = 50;
+        final int BEANS_PER_CUP = 15;
 
         System.out.println("Write how many cups of coffee you will need:");
         Scanner scanner = new Scanner(System.in);
         int cups = scanner.nextInt();
 
         System.out.println("For " + cups + " cups of coffee you will need:");
-        System.out.println(cups * waterPerCup + " ml of water");
-        System.out.println(cups * milkPerCup + " ml of milk");
-        System.out.println(cups * coffeeBeansPerCup + " g of coffee beans");
+        System.out.println(cups * WATER_PER_CUP + " ml of water");
+        System.out.println(cups * MILK_PER_CUP + " ml of milk");
+        System.out.println(cups * BEANS_PER_CUP + " g of coffee beans");
     }
 }
